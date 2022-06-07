0b5e145f19082a8a49a7dd016ef2d306130a745b
Jose Nortes <nooors@gmail.com>
2022-06-07 21:46:15 +0200

    weather component and module; search component and module sets; angular.json set to indicate path to sass preprocesor, reset styles and variables set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SearchModule } from "./components/search/search.module";
+import { WeatherModule } from "./pages/weather/weather.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SearchModule, WeatherModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
