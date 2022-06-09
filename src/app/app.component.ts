5dd958ff0aecf7a9b8449ab4aede9aa2e8f69483
Jose Nortes <nooors@gmail.com>
2022-06-09 07:14:15 +0200

    Ready to build services in order to call the API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'weather-angular-app';
+  title = "weather-angular-app";
+
+  public weather$!: Observable<any>;
+
+  public onSearch(search: string): void {
+    console.log(search);
+  }
 }
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { debounceTime, distinctUntilChanged, tap, map, filter } from "rxjs";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  selector: "app-search",
+  template: `
+    <div class="search">
+      <input
+        type="text"
+        class="search__input"
+        placeholder="City..."
+        [formControl]="inputSearch"
+      />
+    </div>
+  `,
+  styleUrls: ["./search.component.scss"],
 })
 export class SearchComponent implements OnInit {
+  inputSearch = new FormControl("");
+  @Output() submitted = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.onChange();
   }
-
+  private onChange(): void {
+    this.inputSearch.valueChanges
+      .pipe(
+        map((search: string) => search.trim()),
+        debounceTime(850),
+        distinctUntilChanged(),
+        filter((search: string) => search !== ""),
+        tap((search: string) => this.submitted.emit(search)),
+      )
+      .subscribe();
+  }
 }
