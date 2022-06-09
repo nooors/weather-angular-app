4abc1ce34adb03b1998a4fe7bad7031db9fbd71d
Jose Nortes <nooors@gmail.com>
2022-06-09 16:03:13 +0200

    app works properly, geolocation feature lefts

---
+++ src/app/shared/interfaces/services/geo-location.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({ providedIn: "root" })
+export class GeoLocationService {
+  public getCurrentPosition(): Promise<any> {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 3000,
+      maximumAge: 0,
+    };
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+  }
+}
